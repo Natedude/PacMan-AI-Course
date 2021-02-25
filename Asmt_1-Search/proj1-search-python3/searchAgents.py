11b59148327e18d58836c35d51941e911f2f5627
Natedude <diggity123@gmail.com>
2021-02-25 06:08:03 -0800

    fin q8

--- Asmt_1-Search/proj1-search-python3/searchAgents.py
+++ Asmt_1-Search/proj1-search-python3/searchAgents.py
@@ -579,7 +579,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.breadthFirstSearch(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -615,7 +615,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
