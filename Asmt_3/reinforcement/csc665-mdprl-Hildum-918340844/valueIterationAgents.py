c1a1cf6c71d31784965004608acb73c86478c3ea
Natedude <diggity123@gmail.com>
2021-04-07 05:37:46 -0700

    add zip

---
+++ Asmt_3/reinforcement/csc665-mdprl-Hildum-918340844/valueIterationAgents.py
@@ -0,0 +1,203 @@
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+"""
+startState: (0, 0)
+values (after looking up startState): {(0, 0): 0}
+startActions: ('north', 'west', 'south', 'east')
+states: ['TERMINAL_STATE', (0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)]
+numStates: 12
+nextState and Prob lists for each action from startStart:
+[[((0, 1), 0.8), ((0, 0), 0.1), ((1, 0), 0.1)],
+ [((0, 0), 0.9), ((0, 1), 0.1)],
+ [((0, 0), 0.9), ((1, 0), 0.1)],
+ [((1, 0), 0.8), ((0, 1), 0.1), ((0, 0), 0.1)]]
+"""
+
+import mdp, util
+from mdp import MarkovDecisionProcess
+from pprint import pprint
+
+from learningAgents import ValueEstimationAgent
+import collections
+
+
+class ValueIterationAgent(ValueEstimationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+
+    def __init__(self, mdp : MarkovDecisionProcess, discount=0.9, iterations=100):
+        """
+        Your value iteration agent should take an mdp on
+        construction, run the indicated number of iterations
+        and then act according to the resulting policy.
+
+        Some useful mdp methods you will use:
+            mdp.getStates()
+            mdp.getPossibleActions(state)
+            mdp.getTransitionStatesAndProbs(state, action)
+            mdp.getReward(state, action, nextState)
+            mdp.isTerminal(state)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter()  # A Counter is a dict with default 0
+        for i in range(iterations):
+            self.runValueIteration()
+
+    def runValueIteration(self):
+        # Write value iteration code here
+        "*** YOUR CODE HERE ***"
+        values = self.values.copy()
+        states = self.mdp.getStates()
+        for s in states:
+            values[s] = self.best(s)[0]
+        self.values = values
+
+    def getValue(self, state):
+        """
+        Return the value of the state (computed in __init__).
+        """
+        return self.values[state]
+
+    def computeQValueFromValues(self, state, action):
+        """
+        Compute the Q-value of action in state from the
+        value function stored in self.values.
+        """
+        "*** YOUR CODE HERE ***"
+        wAv = 0 #weighted average
+        nextStateAndProbPairList = \
+            self.mdp.getTransitionStatesAndProbs(state,action)
+        for transition in nextStateAndProbPairList:
+            # t[ r + V_k-1(s') ] and add to wAv
+            sPrime, t = transition
+            # V_k-1(s')
+            valPrev = self.values[sPrime]
+            r = self.mdp.getReward(state,action,sPrime)
+            wAv += t * (r + self.discount * valPrev)
+        return wAv
+
+    def computeActionFromValues(self, state):
+        """
+        The policy is the best action in the given state
+        according to the values currently stored in self.values.
+
+        You may break ties any way you see fit.  Note that if
+        there are no legal actions, which is the case at the
+        terminal state, you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        return self.best(state)[1]
+
+    def best(self, state) -> tuple:
+        """
+        Compute Q value, equivalent to a weighted average
+        with probabilities as weights,
+        for a state and each of its possible actions.
+        Return max Q-value tuple (Q-value,action).
+        """
+        actions = self.mdp.getPossibleActions(state)
+        if len(actions) == 0:
+            return (0, None)
+        wAvList = []
+        for a in actions:
+            wAvList.append((self.computeQValueFromValues(state, a),a))
+        wAvList.sort(reverse=True)
+        return wAvList[0]
+
+    def getPolicy(self, state):
+        return self.computeActionFromValues(state)
+
+    def getAction(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.computeActionFromValues(state)
+
+    def getQValue(self, state, action):
+        return self.computeQValueFromValues(state, action)
+
+
+class AsynchronousValueIterationAgent(ValueIterationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        An AsynchronousValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs cyclic value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+
+    def __init__(self, mdp, discount=0.9, iterations=1000):
+        """
+        Your cyclic value iteration agent should take an mdp on
+        construction, run the indicated number of iterations,
+        and then act according to the resulting policy. Each iteration
+        updates the value of only one state, which cycles through
+        the states list. If the chosen state is terminal, nothing
+        happens in that iteration.
+
+        Some useful mdp methods you will use:
+            mdp.getStates()
+            mdp.getPossibleActions(state)
+            mdp.getTransitionStatesAndProbs(state, action)
+            mdp.getReward(state)
+            mdp.isTerminal(state)
+        """
+        ValueIterationAgent.__init__(self, mdp, discount, iterations)
+
+    def runValueIteration(self):
+        "*** YOUR CODE HERE ***"
+
+
+class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A PrioritizedSweepingValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs prioritized sweeping value iteration
+        for a given number of iterations using the supplied parameters.
+    """
+
+    def __init__(self, mdp, discount=0.9, iterations=100, theta=1e-5):
+        """
+        Your prioritized sweeping value iteration agent should take an mdp on
+        construction, run the indicated number of iterations,
+        and then act according to the resulting policy.
+        """
+        self.theta = theta
+        ValueIterationAgent.__init__(self, mdp, discount, iterations)
+
+    def runValueIteration(self):
+        "*** YOUR CODE HERE ***"
+
